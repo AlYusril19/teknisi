d8239e1dec7b5b63c141e92ef72d86aa58fdda47
AlYusril19 <yusrilefendi12345@gmail.com>
2024-11-26 11:01:27 +0700

    Beta-Version-1.4.2 (bugfix biaya kerja mitra)

--- resources/views/layouts/app_sneat_mitra.blade.php
+++ resources/views/layouts/app_sneat_mitra.blade.php
@@ -422,6 +422,15 @@
       }, 3500);
     </script>
 
+    <script>
+      function formatRupiahJS(angka) {
+          var reverse = angka.toString().split('').reverse().join('');
+          var ribuan = reverse.match(/\d{1,3}/g);
+          ribuan = ribuan.join('.').split('').reverse().join('');
+          return 'Rp ' + ribuan;
+      }
+    </script>
+
     @yield('js')
 
     <script>
